e6148ae60cbadd8207eed48e713cfa70eb74adaf
Yaritza <yaritzamicaelleb@gmail.com>
2022-09-29 08:50:15 -0300

    video e email

--- produtos.php
+++ produtos.php
@@ -1,32 +1,50 @@
 <?php
 
 include_once 'conexao.php';
+$result_produtos = "SELECT * FROM produtos";
+$resultado_produtos = mysqli_query($conn, $result_produtos);
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-<link rel="shortcut icon" href="image\alex_logo.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alex DT Churrasqueiras</title>
 
-    <title>Produtos</title>
+    <script src="https://kit.fontawesome.com/11e64fd663.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="script.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="bootstrap\bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js"></script>
+    <link rel="stylesheet" href="bootstrap\bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <div class="container">
 
-        <h1 class="display-4 mt-5 mb-5">Produtos</h1>
+
+		<div class="container theme-showcase" role="main">
+        <div class="page-header">
+        <h1>Produtos</h1>
+        </div>
+
+    <div class="row">
+
         <?php
 
         $query_produtos = "SELECT id,nome,descricao,foto FROM produtos ORDER BY id DESC";
         $result_produtos = $conn->query($query_produtos);
         ?>
 
-        <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div>
 
             <?php
             while ($dado_produto = mysqli_fetch_assoc($result_produtos)) {
@@ -38,26 +56,28 @@ include_once 'conexao.php';
                 echo "DESCRIÇÃO: $descricao <br>";
                 echo "<hr>";*/
             ?>
-                <div class="col mb-4 text-center">
-                    <div class="card">
-                        <img src='<?php echo "image/produtos/$id/$foto" ?>' class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $nome; ?></h5>
-                            <p class="card-text"><?php echo $descricao; ?></p>
-                            <a href="https://wa.me/5513996592947?text=Ol%C3%A1%2C+Alex+DT+Churrasqueira%21" target="_blank"class="btn btn-primary">Peça o seu!</a>
-                            <a href="detalhes_produtos.php?id=<?php echo $id;?>" class="btn btn-primary">Detalhes</a>
-
-                        </div>
-                    </div>
-                <?php
-            }
-                ?>
-                </div>
+               		<div class="row">
+					<div class="col-sm-6 col-md-4">
+						<div class="thumbnail">
+							<img src="<?php echo "image/produtos/$id/$foto" ?>" alt="...">
+							<div class="caption text-center">
+								<a><h3><?php echo $nome; ?></h3></a>
+								<p><a href="#" class="btn btn-primary" role="button">Comprar</a> </p>
+							</div>
+						</div>
+					</div>
+				<?php } ?>
+			</div>
+            <?php
+            ?>
         </div>
 
-        
+
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
 </body>
-
+   <!--sessão  footer-->
+   <footer>
+        <span>Criado por <a href="https://github.com/YaritzaSilva" target="_blank">YaritzaSilva</a> | 2022</span>
+    </footer>
 </html>
