a7c228945d4586187aaf3947e5fb21344949ae4f
Yaritza <yaritzamicaelleb@gmail.com>
2022-10-02 20:30:31 -0300

    novas img

--- script.js
+++ script.js
@@ -31,3 +31,18 @@ $(document).ready(function(){
     });
   
 });
+function clique(img){
+    var modal=document.getElementById('janelaModal');
+    var modalImg=document.getElementById("imgModal");
+    var captionTexto=document.getElementById("txtImg");
+    var btFechar=document.getElementById("fechar");
+
+    modal.style.display="block";
+    modalImg.src=img.src;
+    modalImg.alt=img.alt;
+    captionTexto.innerHTML=img.alt;
+
+    btFechar.onclick=function(){
+          modal.style.display="none";
+    }
+}
